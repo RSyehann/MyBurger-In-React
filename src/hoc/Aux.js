f6a954278920a55580a038f137ff4d465ebd49af
Ryan Shen <ryanshen@Ryande-MacBook-Pro.local>
2018-11-19 20:12:28 +0800

    rdelete unnecessary code in Aux.js file

--- src/hoc/Aux.js
+++ src/hoc/Aux.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 const Aux = ( props ) => props.children;
 
 export default Aux;
-=======
-const Aux = ( props ) => props.children;
-
-export default Aux;
->>>>>>> e961e0b6cb3684955bd95a37a7bafea5e22076b6
