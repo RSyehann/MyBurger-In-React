d2344e890b30af0eecf09da7989c4e06b93ad076
Ryan Shen <ryanshen@Ryande-MacBook-Pro.local>
2018-11-19 19:50:36 +0800

    create a Layout and hoc in src folder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Aux from '../../c'
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
