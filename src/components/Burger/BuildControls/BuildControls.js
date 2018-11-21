12cb921213386d48e15f9c9ac4ec994b36fe0e9d
Ryan Shen <ryanshen@Ryande-MacBook-Pro.local>
2018-11-21 20:22:12 +0800

    add addIngredient and removeIngredient handlers

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './BuildControls.css';
+import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
@@ -11,12 +11,16 @@ const controls = [
 ];
 
 const buildControls = (props) => (
-    <div className={"BuildControls"}> 
+    <div className={classes.BuildControls}> 
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]} />
         ))}
     </div>
 );
-
 
 export default buildControls;
