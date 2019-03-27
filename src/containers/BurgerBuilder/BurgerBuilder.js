ad8ffd62b80b8aaa7ae251e6a8ead1327d407b84
Ryan Shen <ryanshen@Ryande-MacBook-Pro.local>
2019-03-27 23:15:14 +0800

    add a method in BurgerBuilder to navigate to the checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,6 +32,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount () {
+        console.log(this.props);
         axios.get('https://burger-builder-73296.firebaseio.com/ingredients.json')
             .then(response => {
                 this.setState({ingredients: response.data});
@@ -96,28 +97,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!')
-        this.setState( { loading: true } );
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Xavier Shen',
-                address: {
-                    street: 'RunningStreet 1',
-                    zipcode: '41351',
-                    country: 'Taiwan'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState( { loading: false , purchasing: false } );
-            })
-            .catch(error => {
-                this.setState( { loading: false , purchasing: false } );
-            });
+        // this.setState( { loading: true } );
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Xavier Shen',
+        //         address: {
+        //             street: 'RunningStreet 1',
+        //             zipcode: '41351',
+        //             country: 'Taiwan'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState( { loading: false , purchasing: false } );
+        //     })
+        //     .catch(error => {
+        //         this.setState( { loading: false , purchasing: false } );
+        //     });
+        this.props.history.push('/checkout');
     }
 
     render() {
